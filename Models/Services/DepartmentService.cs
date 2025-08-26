e36901ce9142f0a51d9da626c463cb6535cfb9f6
Lucas Fernandes <lcsfernandesg@gmail.com>
2025-08-26 19:27:30 -0300

    Implementing simple search

--- Models/Services/DepartmentService.cs
+++ Models/Services/DepartmentService.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Linq;
 using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMVC.Models.Services
